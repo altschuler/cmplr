4e2169725647f08799958b405ba8a9956dd41133
Simon Altschuler <simon@altschuler.dk>
2013-06-23 23:44:30 +0200

    isolated driver code. renamed binary to wtf, which now parses files instead of being a REPL

---
+++ Driver.cpp
@@ -0,0 +1,76 @@
+#include "Driver.hpp"
+
+void Driver::HandleDefinition() {
+  FunctionAST *func = TheParser.ParseDefinition();
+  Function *code = Gen->Generate(func);
+  if (func && code) {
+	//	fprintf(stderr, "Function '%s' defined.\n", func->GetPrototype()->GetName().c_str());
+  }
+  else
+	TheParser.GetNextToken();
+}
+
+void Driver::HandleOperator() {
+  OperatorAST *func = TheParser.ParseOperator();
+  Function *code = Gen->Generate(func);
+  if (func && code) {
+	//	  fprintf(stderr, "Operator '%c' defined.\n", func->GetOp());
+  }
+  else
+	TheParser.GetNextToken();
+}
+
+void Driver::HandleExtern() {
+  PrototypeAST *ext = TheParser.ParseExtern();
+  Function *code = Gen->Generate(ext);
+  if (ext && code) {
+	//	fprintf(stderr, "Extern '%s' defined.\n", ext->GetName().c_str());
+  }
+  else
+	TheParser.GetNextToken();
+}
+
+void Driver::HandleTopLevelExpr() {
+  FunctionAST *expr = TheParser.ParseTopLevelExpr();
+  Function *code = Gen->Generate(expr);
+  if (expr && code) {
+	void *funcPtr = Gen->GetExecEngine()->getPointerToFunction(code);
+
+	double (*fptr)() = (double (*)())(intptr_t)funcPtr;
+	cout << fptr() << endl;
+  }
+  else
+	TheParser.GetNextToken();
+}
+
+void Driver::Go(string file) {
+  TheParser.SetInputFile(file);
+
+  TheParser.GetNextToken();
+
+  while(1) {
+	switch(TheParser.GetCurTok()) {
+	case tok_eof: return;
+	case tok_def: 	  
+	  //	  fprintf(stderr, "= ");
+	  HandleDefinition(); 
+	  break;
+	case tok_extern: 
+	  //	  fprintf(stderr, "= ");
+	  HandleExtern(); 
+	  break;
+	case tok_op: 
+	  //	  fprintf(stderr, "= ");
+	  HandleOperator(); 
+	  break;
+	case ';': 
+	  //	  fprintf(stderr, ">");
+	  TheParser.GetNextToken(); 
+	  break; // eat ;
+	default: 
+	  //	  fprintf(stderr, "= ");
+	  HandleTopLevelExpr(); 
+	  break;
+	}
+  }
+}
---
+++ Driver.hpp
@@ -0,0 +1,36 @@
+#include "llvm/ExecutionEngine/ExecutionEngine.h"
+#include "llvm/ExecutionEngine/JIT.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/LLVMContext.h"
+#include "llvm/Module.h"
+
+#include "boost/format.hpp"
+
+#include "Parser.hpp"
+#include "Errors.hpp"
+#include "Codegen.hpp"
+
+#include <iostream>
+
+#ifndef DRIVER_HPP
+#define DRIVER_HPP
+
+using namespace std;
+using namespace llvm;
+
+class Driver {
+  Parser TheParser;
+  Codegen *Gen;
+
+public:
+  Driver(Codegen *codegen) : Gen(codegen) {}
+  void Go(string file);
+
+private:
+  void HandleDefinition();
+  void HandleOperator();
+  void HandleExtern();
+  void HandleTopLevelExpr();
+};
+
+#endif
